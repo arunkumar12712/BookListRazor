bc34b5c696c759fe33d51b52f6e96367efe1c23b
arun kumar <arunkumar12712@gmail.com>
2020-08-17 15:45:05 +0530

    initalchecking

---
+++ BookListRazor/Pages/BookList/Update.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor
+{
+    public class UpdateModel : PageModel
+    {
+        protected readonly ApplicationDBContext _db;
+        public UpdateModel(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            Book = new Book();
+            if (id == null)
+            {
+                return Page();
+            }
+            Book = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if(Book == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                if(Book.Id == 0)
+                {
+                    _db.Book.Add(Book);
+                }
+                else
+                {
+                    _db.Book.Update(Book);
+                }
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
